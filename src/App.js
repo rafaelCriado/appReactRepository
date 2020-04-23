429fb2dd95d20c2fc6141e9e80f31b8d8e758263
Rafael Criado <rafael.marquescriado@gmail.com>
2020-04-23 15:20:59 -0300

    Feat: List, add and delete repositories

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from "./services/api";
+
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+
+  useEffect(()=>{
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  },[]);
+
   async function handleAddRepository() {
-    // TODO
+    api.post('repositories',{
+      title: `Title ${new Date()}`,
+      url: `http://www.globo.com.br`,
+      techs: ['Node', 'React']
+    }).then(response =>{
+      setRepositories([...repositories, response.data])
+    });
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+    api.delete(`repositories/${id}`).then(response => {
+      if(response.status === 204){
+        // removido com sucesso
+        let repos = repositories.filter(r => r.id !== id);
+        setRepositories(repos);
+      }
+    })
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => {
+          return (
+              <li key={repository.id}>
+                {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+                <button onClick={() => handleRemoveRepository(repository.id)}>
+                  Remover
+                </button>
+              </li>
+          );
+        })}        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
